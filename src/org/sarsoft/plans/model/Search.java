f35cf59f977002f6e977f79a83fb9cd994296f62
matt <matt@mattj.net>
2011-01-18 05:42:46 +0000

    Range Rings

--- src/org/sarsoft/plans/model/Search.java
+++ src/org/sarsoft/plans/model/Search.java
@@ -2,9 +2,13 @@ package org.sarsoft.plans.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
+import org.hibernate.annotations.Cascade;
 import org.sarsoft.common.model.JSONAnnotatedEntity;
 import org.sarsoft.common.model.JSONSerializable;
+import org.sarsoft.common.model.Waypoint;
 
 @JSONAnnotatedEntity
 @Entity
@@ -12,6 +16,7 @@ public class Search {
 
 	private String name;
 	private String mapConfig;
+	private Waypoint plk;
 
 	public void setName(String name) {
 		this.name = name;
@@ -29,5 +34,15 @@ public class Search {
 	@JSONSerializable
 	public String getMapConfig() {
 		return mapConfig;
+	}
+
+	@ManyToOne
+	@Cascade({org.hibernate.annotations.CascadeType.ALL})
+	public Waypoint getPlk() {
+		return plk;
+	}
+
+	public void setPlk(Waypoint plk) {
+		this.plk = plk;
 	}
 }
