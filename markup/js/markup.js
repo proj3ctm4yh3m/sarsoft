433dd2045ca780bb7995796b8c71ec7be2f1d31e
matt <matt@mattj.net>
2013-03-22 19:49:06 +0000

    Ghost marker appears when editing marker 0, as the waypoint ID gets incremented to 1.

--- markup/js/markup.js
+++ markup/js/markup.js
@@ -28,7 +28,7 @@ org.sarsoft.ShapeDAO.prototype.sanitize = function(obj) {
 	obj.weight=1*obj.weight;
 	obj.fill=1*obj.fill;
 	if(this.offline && obj.way != null) {
-		if(obj.way.id == null | obj.way.id == 0) {
+		if(obj.way.id == null) {
 			obj.way.id = this.nextId++;
 		} else {
 			this.nextId = Math.max(obj.way.id+1, this.nextId); 
@@ -95,7 +95,7 @@ org.sarsoft.MarkerDAO.prototype = new org.sarsoft.BaseDAO(true);
 
 org.sarsoft.MarkerDAO.prototype.sanitize = function(obj) {
 	if(this.offline && obj.position != null) {
-		if(obj.position.id == null || obj.position.id == 0) {
+		if(obj.position.id == null) {
 			obj.position.id = this.nextId++;
 		} else {
 			this.nextId = Math.max(obj.position.id+1, this.nextId);
