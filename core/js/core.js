3cc831c691bd2820944020dd713cdffc79fe00bd
matt <matt@mattj.net>
2013-10-03 17:57:39 +0000

    Security exception when trying to read cross-domain stylesheets due to some changes in the Google Maps code.

--- core/js/core.js
+++ core/js/core.js
@@ -1347,8 +1347,10 @@ org.sarsoft.view.MapSizeForm.prototype.write = function() {
 org.sarsoft.view.MapSizeForm.prototype._getMarginRule = function() {
 	for(var i = 0; i < document.styleSheets.length; i++) {
 		var sheet = document.styleSheets[i];
-		var rules = sheet.cssRules;
-		if(rules == null) rules = sheet.rules;
+		try {
+			var rules = sheet.cssRules;
+			if(rules == null) rules = sheet.rules;
+		} catch(e) {}
 		if(rules != null) for(var j = 0; j < rules.length; j++) {
 			if(rules[j].cssText != null && rules[j].cssText.indexOf("@page") >= 0 && rules[j].cssText.indexOf("margin") >= 0) return rules[j];
 		}
